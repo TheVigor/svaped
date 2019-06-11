d808e8c60d4cf5515018907f53f1f981fde71714
WandererN <izhdanovn@gmail.com>
2019-06-11 11:10:16 +0300

    NetworkConstants initially added

---
+++ app/src/main/java/com/xoxoton/svaped/network/NetworkConstants.kt
@@ -0,0 +1,15 @@
+package com.xoxoton.svaped.network
+
+object NetworkConstants {
+    const val KEY_CODE = "code"
+    const val KEY_INDUSTRY_ID = "industryId"
+    const val KEY_INDUSTRY_TYPE = "industryType"
+    const val KEY_PASSWORD = "password"
+    const val KEY_PHONE = "phone"
+    const val KEY_PHONE_CODE = "phoneCode"
+    const val KEY_REQUEST_TYPE = "requestType"
+
+    const val REQUEST_PHONE_CONFIRMATION = "10023"
+    const val REQUEST_REGISTRATION = "10022"
+    const val REQUEST_VERIFICATION_CODE = "50002"
+}
