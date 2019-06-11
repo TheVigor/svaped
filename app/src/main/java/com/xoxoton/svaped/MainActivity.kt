7280ef1781ff3fc8f92adcd86a308dd2488d471f
TheVigor <igor.proskuryakov14@gmail.com>
2019-06-11 11:01:37 +0300

    inited empty kotlin android project

---
+++ app/src/main/java/com/xoxoton/svaped/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.xoxoton.svaped
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
