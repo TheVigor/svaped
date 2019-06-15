4fa7d31b3df3007b3fefe5d4cad2b13e4d9f483a
pi <pi>
2019-06-15 20:10:54 +0300

    added dependencies versions based on buildSrc kotlin-dsl

---
+++ buildSrc/src/main/java/Dependencies.kt
@@ -0,0 +1,44 @@
+object ApplicationId {
+    val id = "com.xoxoton.svaped"
+}
+
+object Modules {
+    val app = ":app"
+}
+
+object Releases {
+    val versionCode = 1
+    val versionName = "1.0"
+}
+
+object Versions {
+    val kotlin = "1.3.31"
+    val gradle = "3.4.1"
+
+    val compileSdk = 28
+    val minSdk = 21
+    val targetSdk = 28
+
+    val material = "1.0.0"
+
+    val lifecycleExtensions = "2.0.0"
+    val lifecycleCompiler = "2.0.0"
+
+    val appCompat = "1.0.2"
+    val coreKtx = "1.0.2"
+
+    val constraintLayout = "1.1.3"
+
+    val junit = "4.12"
+    val androidTestRunner = "1.2.0"
+    val espressoCore = "3.2.0"
+
+    val rxKotlin = "2.3.0"
+    val rxAndroid = "2.1.1"
+
+    val retrofit = "2.5.0"
+    val retrofitGson = "2.5.0"
+    val retrofitRxJava = "2.5.0"
+
+    val yandexMapKit = "3.3.1"
+}
